85f98a4ff5dcf47421004f4b9b51bc557a43d709
lucaberto95 <49396212+lucaberto95@users.noreply.github.com>
2019-04-08 14:37:32 +0200

    0.0.0.08

--- app.py
+++ app.py
@@ -15,12 +15,17 @@ class FilterLanza(BaseFilter):
     def filter(self, message):
         return 'Lanza?' in message.text
 
+class FilterPrato(BaseFilter):
+    def filter(self, message):
+        return 'Prato' in message.text
+
+
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
     """Send a message when the command /start is issued."""
-    context.message.reply_text('Hi!')
-    
+    context.message.reply_text('Sono tornato merde!')
+
 def taccuino(update, context):
     listaout= "\n".join(str(x) for x in ammoniti)
     context.message.reply_text("Ecco il taccuino dell'arbitro:\n\n" + listaout)
@@ -31,13 +36,8 @@ def isAdministrator(update, context):
     elif(update.get_chat_member(context.message.chat_id, context.message.reply_to_message.from_user.id).status == "administrator"):
         context.message.reply_text("Un amministratore")
     else: context.message.reply_text("Nope, persona inutile")
-#    administratorList = update.get_chat_administrators(context.message.chat_id)
-#    print(administratorList)
-#    administratorListString = list()
-#    for item in administratorList:
-#        administratorListString.append(administratorList[item.user.last_name])
-#    print(administratorList)
-    
+
+
 def ammonito(update, context):
     if(update.get_chat_member(context.message.chat_id, context.message.from_user.id).status == "creator" or update.get_chat_member(context.message.chat_id, context.message.from_user.id).status == "administrator"):
         global ammoniti
@@ -55,11 +55,11 @@ def ammonito(update, context):
 
 def espulso(update, context):
     if(update.get_chat_member(context.message.chat_id, context.message.from_user.id).status == "creator" or update.get_chat_member(context.message.chat_id, context.message.from_user.id).status == "administrator"):
-        if(context.message.reply_to_message.from_user.is_bot == False):
+        if(context.message.reply_to_message.from_user.is_bot == False and context.message.reply_to_message.from_user.last_name != "Lanzarini"):
             context.message.reply_text("Ma no! Cosa e' preso a " + context.message.reply_to_message.from_user.first_name + " " + context.message.reply_to_message.from_user.last_name + "!\nL'arbitro si e' trovato costretto a sventolare il cartellino rosso!\nOra " + context.message.reply_to_message.from_user.last_name + " sara' costretto ad abbandonare il campo")
             update.kick_chat_member(context.message.chat_id, context.message.reply_to_message.from_user.id)
-                    
-def grazia(update, context):  
+
+def grazia(update, context):
     if(update.get_chat_member(context.message.chat_id, context.message.from_user.id).status == "creator" or update.get_chat_member(context.message.chat_id, context.message.from_user.id).status == "administrator"):
         global ammoniti
         if context.message.reply_to_message.from_user.last_name in ammoniti:
@@ -75,7 +75,7 @@ def help(update, context):
 def max(update, context):
     """Send a message when the command /help is issued."""
     context.message.reply_text('Se continui Max ti riempie di Bot')
-    
+
 def chatID(update, context):
 #    context.message.reply_text(context.message.reply_to_message.from_user.id)
     context.message.reply_text("Ciao, " + context.message.from_user.first_name + "!\nIl tuo chat id in questo gruppo e' " +  str(context.message.from_user.id))
@@ -85,9 +85,8 @@ def lanza(update, context):
     lanzaflex=lanzaflex+2
     context.message.reply_text("Lanza sta sollevando " + str(lanzaflex) + " kg")
 
-    """def echo(context, update):"""
-    """update.message.reply_text(update.message.text)"""
-
+def prato(update, context):
+    context.message.reply_text("Forse volevi dire Rovigo")
 
 def error(update, context):
     """Log Errors caused by Updates."""
@@ -96,33 +95,19 @@ def error(update, context):
 
 if __name__ == '__main__':
     # Getting mode, so we could define run function for local and Heroku setup
-    mode = "dev"
     TOKEN = "847018555:AAHOHfC7Nj7zLMEo5leHE2knXoLZpnoifv4"
     updater = Updater(TOKEN)
-    if mode == "dev":
-        def run(updater):
+
+    def run(updater):
             updater.start_polling(clean=True)
-    elif mode == "prod":
-        def run(updater):
-            PORT = int("8443")
-            HEROKU_APP_NAME = "doublecheckbot"
-        # Code from https://github.com/python-telegram-bot/python-telegram-bot/wiki/Webhooks#heroku
-            updater.start_webhook(listen="0.0.0.0",
-                                  port=PORT,
-                                  url_path=TOKEN)
-            updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TOKEN))
-    else:
-        logger.error("No MODE specified!")
-        sys.exit(1)
     logger.info("Starting bot")
     """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
+
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
     filter_lanza= FilterLanza()
+    filter_prato=FilterPrato()
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
@@ -133,22 +118,13 @@ if __name__ == '__main__':
     dp.add_handler(CommandHandler("isAdministrator", isAdministrator))
     dp.add_handler(CommandHandler("grazia", grazia))
     dp.add_handler(CommandHandler("espulso", espulso))
-    #lanzastr="Lanza"
+
     dp.add_handler(MessageHandler(filter_lanza, lanza))
+    dp.add_handler(MessageHandler(filter_prato, prato))
 
-    #dp.add_handler(CommandHandler("member", member))
-    #dp.add_handler(MessageHandler(Filters.text, lanza))
-
-    # on noncommand i.e message - echo the message on Telegram
-    #dp.add_handler(MessageHandler(Filters.text, echo))
 
     # log all errors
     dp.add_error_handler(error)
 
-    # Start the Bot
-#    updater.start_polling(clean=True)
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     run(updater)
